8adab481088a7d55ed8e238c7960e5986b22eede
suhasrkms <suhasrkms@gmail.com>
2023-07-18 18:50:54 +0530

    Minor improvements

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,36 +1,40 @@
 @extends('layouts.app')
 @section('navbar_home')
-  @if (Route::has('login'))
-          @auth
-            <li class="nav-item">
-              <a class="nav-link text-dark" href="{{ url('/home') }}">Home</a>
-            </li>
-          @else
-              <li class="nav-item">
-                <a class="nav-link text-dark" href="{{ route('login') }}">Login</a>
-              </li>
+@if (Route::has('login'))
+@auth
+<li class="nav-item">
+  <a class="nav-link text-dark" href="{{ url('/home') }}">Home</a>
+</li>
+@else
+<li class="nav-item">
+  <a class="nav-link text-dark" href="{{ route('login') }}">Login</a>
+</li>
 
-              @if (Route::has('register'))
-                  <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('register') }}">Register</a>
-                  </li>
-              @endif
-          @endauth
-    @else
-  @endif
+@if (Route::has('register'))
+<li class="nav-item">
+  <a class="nav-link text-dark" href="{{ route('register') }}">Register</a>
+</li>
+@endif
+@endauth
+@else
+@endif
 @endsection
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">{{ __('Welcome') }}</div>
 
-                <div class="card-body">
-                    {{ __('You are in home !') }}
-                </div>
-            </div>
+        <div class="card-body">
+          <div class="container text-center mt-5">
+            <h1 class="display-5">Welcome to our <a href="https://github.com/suhasrkms/Lara-Fire">Lara-Fire</a> <span>&#127881;</span></h1>
+            <p class="lead">We've seamlessly integrated Firebase into our platform, enabling powerful features like <span class="text-danger">Firestore</span>, <span class="text-danger">Authentication</span>, <span class="text-danger">Email Verification</span>, and an <span class="text-danger">Admin Panel</span>. With Firebase, we ensure real-time data updates, secure user authentication, and efficient administration. Join us as we leverage Firebase to deliver an exceptional user experience.</p>
+          </div>
+
         </div>
+      </div>
     </div>
+  </div>
 </div>
 @endsection
